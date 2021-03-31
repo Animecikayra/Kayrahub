c4e0f5cfa30520735310ff75e6668f96d3c5e87e
Kayra <70236944+Kayradeveloper@users.noreply.github.com>
2021-03-31 10:41:41 +0300

    Update Kayrahub.lua

--- Kayrahub.lua
+++ Kayrahub.lua
@@ -38,6 +38,8 @@ elseif game.PlaceId == 5835263912 then -- Anime Mania --
 loadstring(game:HttpGet("http://skyhubking.xyz/script/free_script/OPM.lua"))()
 elseif game.PlaceId == 221718525 then -- Ninja Tycoon
 loadstring(game:HttpGet("https://reativ.ru/Scripts/Free/NT.lua", true))()
+elseif game.PlaceId == 1962086868 then -- tower of hell
+loadstring(game:HttpGet("raw.githubusercontent.com/Kayradeveloper/Kayrahub/main/Kayrahubtowerofhell.lua", true))()
 else
     game.Players.LocalPlayer:Kick("Kayrahub not support your game :(")
 end
