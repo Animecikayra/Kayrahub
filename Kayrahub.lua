837beea202dd49f093bf92fafbcdd001f1713f9c
Kayra <70236944+Kayradeveloper@users.noreply.github.com>
2021-03-31 06:09:09 +0300

    Update Kayrahub.lua

--- Kayrahub.lua
+++ Kayrahub.lua
@@ -1,7 +1,41 @@
 if game.PlaceId == 4616652839 then --Shindo life
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Kayradeveloper/Kayrahub/main/Kayrahubsindolife'))()
-end
-
-if game.PlaceId == 286090429 then --arsenal
+elseif game.PlaceId == 286090429 then --arsenal
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Kayradeveloper/Kayrahub/main/Kayrahubarsenal'))()
+elseif game.PlaceId == 3400631762 then -- JJB
+loadstring(game:HttpGet("http://skyhubking.xyz/script/free_script/JJB.lua"))()
+elseif game.PlaceId == 4313854107 then -- JJB ITALY
+loadstring(game:HttpGet("http://skyhubking.xyz/script/free_script/JJB.lua"))()
+elseif game.PlaceId == 5414779423 then --Science Simulator
+loadstring(game:HttpGet("http://skyhubking.xyz/script/free_script/SS.lua"))()
+elseif game.PlaceId == 2753915549 then --BF OLD
+loadstring(game:HttpGet("http://skyhubking.xyz/script/free_script/BF.lua"))()
+elseif game.PlaceId == 4442272183 then --BF NEW
+loadstring(game:HttpGet("http://skyhubking.xyz/script/free_script/BF.lua"))()
+elseif game.PlaceId == 394506555 then --BF NEW
+loadstring(game:HttpGet("http://skyhubking.xyz/script/free_script/GP.lua"))()
+elseif game.PlaceId == 4601350214 then --SL
+loadstring(game:HttpGet("http://skyhubking.xyz/script/free_script/SL.lua"))()
+elseif game.PlaceId == 6382464838 then --TP
+loadstring(game:HttpGet("http://skyhubking.xyz/script/free_script/ThaiPiece.lua"))()
+elseif game.PlaceId == 4934471106 then --MHM
+loadstring(game:HttpGet("http://skyhubking.xyz/script/free_script/Myhero.lua"))()
+elseif game.PlaceId == 5912800471 then --MHM
+loadstring(game:HttpGet("http://skyhubking.xyz/script/free_script/Myhero.lua"))()
+elseif game.PlaceId == 537413528 then -- babft
+loadstring(game:HttpGet("http://skyhubking.xyz/script/free_script/babft.lua"))()
+elseif game.PlaceId == 6186867282 then -- project XXXL --
+loadstring(game:HttpGet("http://skyhubking.xyz/script/free_script/projectxl.lua"))()
+elseif game.PlaceId == 6329844902 then -- Last Piece --
+loadstring(game:HttpGet("http://skyhubking.xyz/script/free_script/lastpiece.lua"))()
+elseif game.PlaceId == 5293755937 then -- Speed Simulator --
+loadstring(game:HttpGet("http://skyhubking.xyz/script/free_script/SRS.lua"))()
+elseif game.PlaceId == 5569431582 then -- Little World --
+loadstring(game:HttpGet("http://skyhubking.xyz/script/free_script/LittleWorld.lua"))()
+elseif game.PlaceId == 6284881984 then -- Anime Mania --
+loadstring(game:HttpGet("http://skyhubking.xyz/script/free_script/AnimeMania.lua"))()
+elseif game.PlaceId == 5835263912 then -- Anime Mania --
+loadstring(game:HttpGet("http://skyhubking.xyz/script/free_script/OPM.lua"))()
+else
+    game.Players.LocalPlayer:Kick("Kayrahub not support your game :(")
 end
